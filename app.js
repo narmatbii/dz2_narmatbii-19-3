0117fb8e8e887a0a4ab1d2be4ee23a4cf9b9103f
narmatbii <narmatkusnazarov656@gmail.com>
2022-11-11 02:47:05 +0300

    dz2

---
+++ app.js
@@ -0,0 +1,68 @@
+/*/ 1 /*/
+
+// const box = document.querySelector('.box')
+//
+//
+// let xPos = 0;
+// let yPos = 0;
+//
+//
+// function moveBlock() {
+//     if (xPos < 400 && yPos == 0){
+//         xPos += 20;
+//         box.style.left = `${xPos}px`
+//         setTimeout(moveBlock,100)
+//     }else if (xPos >= 400 && yPos < 300) {
+//         yPos += 20;
+//         box.style.top = `${yPos}px`
+//         setTimeout(moveBlock,100)
+//     }else if (xPos > 0 && yPos >= 300) {
+//         xPos -= 20;
+//         box.style.left = `${xPos}px`
+//         setTimeout(moveBlock,100)
+//     }else if (xPos == 0 && yPos > 0) {
+//         yPos -= 20;
+//         box.style.top = `${yPos}px`
+//         setTimeout(moveBlock,100)
+//     }
+//
+// }
+//
+// moveBlock()
+
+
+/*/ 2/*/
+
+//
+//
+// let num = 0
+//
+// const startBtn = document.querySelector(`.startTime`)
+// const stopBtn = document.querySelector(`.stopTime`)
+//
+//
+//
+// startBtn.addEventListener("click", ()=> {
+//     startInt = setInterval(() => {
+//         num++
+//         console.log(num)
+//     }, 1000)
+// })
+//
+// stopBtn.addEventListener("click", ()=> {
+//     clearInterval(startInt)
+//     console.clear()
+//     num = 0
+// })
+
+/*/ 3 /*/
+
+// const num = prompt("Введите число")
+// function papa(num){
+//     function mama () {
+//         alert( num * num)
+//
+//     } mama()
+// }
+// papa(num)
+
